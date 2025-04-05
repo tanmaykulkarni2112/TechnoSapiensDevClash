6520fbb25a5d4eb28a77ccce1cfdcd37e7de094f
Vivek Janbandhu <vivekjanbandhu20@gmail.com>
2025-04-05 12:53:11 +0530

    initialized the project and setup tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+function App() {
+ 
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
